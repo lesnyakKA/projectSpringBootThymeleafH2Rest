c009bb15b1c66215775623617597955b1a755e1c
YYegerev <yevgeniy.yegerev@gmail.com>
2023-02-25 13:57:19 +0300

    Добавить /add

--- src/main/java/com/lesnyak/demo/controller/CustomerController.java
+++ src/main/java/com/lesnyak/demo/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,6 +27,17 @@ public class CustomerController {
         List<Customer> customers = customerService.getAll();
         model.addAttribute("customers", customers);
         return "customers";
+    }
+
+//    @PostMapping("/createOrUpdateCustomer")
+//    public String addCustomer(Model model) {
+//
+//    }
+
+    @GetMapping("/add")
+    public String addCustomer(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "add-customer";
     }
 
 }
